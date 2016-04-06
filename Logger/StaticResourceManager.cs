b4895c9c5141c05af3f50df86bd87df4c3d35f62
devgopher <igevdo@mail.ru>
2016-04-06 10:50:47 +0300

    initial

---
+++ Logger/StaticResourceManager.cs
@@ -0,0 +1,78 @@
+﻿/*
+ * Пользователь: igor.evdokimov
+ * Дата: 02.11.2015
+ * Время: 15:28
+ */
+using System;
+using System.Resources;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Global
+{
+	/// <summary>
+	/// A Static (global) resource manager for simple access to .resx and .resource files
+	/// </summary>
+	public static class StaticResourceManager
+	{
+		private static readonly List<ResourceManager> resource_mgrs = 
+			new List<ResourceManager>();
+		
+		private static readonly string appdir = 
+			Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+		
+		private static List<string> FindResourceTitles() {
+			resource_mgrs.Clear();
+			var ret = new List<string>();
+			var resources_dir = Path.Combine(appdir,"Resources");
+			
+			var files = Directory.GetFiles( resources_dir, "*.resources" );
+			const string regex_pattern = @"(.*)\.(.*)\.(.*)";
+			
+			foreach ( var file in files ) {
+				var match = Regex.Match( Path.GetFileName(file), regex_pattern );
+				if ( match.Groups.Count >= 3 ) {
+					var resource_title = match.Groups[1].Value;
+					if ( !ret.Contains( resource_title ) )
+						ret.Add( resource_title );
+				}
+			}
+			return ret;
+		}
+		
+		static StaticResourceManager()
+		{
+			var resource_titles = FindResourceTitles();
+
+			foreach ( var rt in resource_titles ) {
+				var string_manager = ResourceManager.CreateFileBasedResourceManager( rt,
+				                                                                    appdir +
+				                                                                    Path.DirectorySeparatorChar+
+				                                                                    "Resources",
+				                                                                    null
+				                                                                   );
+				resource_mgrs.Add( string_manager );
+			}
+		}
+		
+		public static string GetStringResource( string res_name ) {
+			foreach ( var rm in resource_mgrs ) {
+				var tmp = rm.GetString( res_name );
+				if ( tmp != null )
+					return tmp;
+			}
+			return null;
+		}
+
+		public static Object GetObjectResource( string res_name ) {
+			foreach ( var rm in resource_mgrs ) {
+				var tmp = rm.GetObject( res_name );
+				if ( tmp != null )
+					return tmp;
+			}
+			return null;
+		}
+	}
+}
---
+++ LoggerTest/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * User: igor.evdokimov
+ * Date: 20.02.2016
+ * Time: 12:01
+ */
+using System;
+using System.Threading;
+
+namespace LoggerTest
+{
+	class Program
+	{
+		static Logger.Logger logger = null;
+		private static void ThreadProc( object state ) {
+			int thread_number = (int)state;
+			for ( int k =0 ; k < 100 ; ++k) {
+				logger.WriteEntry("THREAD #"+thread_number.ToString());
+				logger.WriteError("THREAD #"+thread_number.ToString());
+				logger.WriteWarning("THREAD #"+thread_number.ToString());
+				logger.WriteDebug("THREAD #"+thread_number.ToString());
+			}
+		}
+		
+		public static void Main(string[] args)
+		{
+			logger = Logger.Logger.GetInstance(".", "out.log");
+			int threads_cnt = 200;
+			Logger.Logger.IsSingle = true;
+			Console.WriteLine("A multithread test for Logger!");
+			for ( int i = 0 ; i < threads_cnt; ++i ) {
+				var ts = new ParameterizedThreadStart(ThreadProc);
+				var new_thr = new Thread( ts );
+				
+				new_thr.Start( i );
+			}
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
