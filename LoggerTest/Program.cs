1da27423612cbb1244024da897ed2c9e13b9b739
devgopher <igevdo@mail.ru>
2016-04-15 14:23:31 +0300

    LoggerTest was slightly modified

--- LoggerTest/Program.cs
+++ LoggerTest/Program.cs
@@ -18,23 +18,35 @@ namespace LoggerTest
 				logger.WriteError("THREAD #"+thread_number.ToString());
 				logger.WriteWarning("THREAD #"+thread_number.ToString());
 				logger.WriteDebug("THREAD #"+thread_number.ToString());
+				logger.WriteDebug("THREAD #"+thread_number.ToString()+" k= "+k.ToString());
 			}
 		}
 		
 		public static void Main(string[] args)
 		{
 			logger = Logger.Logger.GetInstance(".", "out.log");
-			int threads_cnt = 200;
+			int threads_cnt = 100;
+			
+			Thread[] threads = new Thread[threads_cnt];
+			System.Threading.ThreadPool.SetMaxThreads( 30 * Environment.ProcessorCount, 2 );
 			Logger.Logger.IsSingle = true;
 			Console.WriteLine("A multithread test for Logger!");
 			for ( int i = 0 ; i < threads_cnt; ++i ) {
 				var ts = new ParameterizedThreadStart(ThreadProc);
 				var new_thr = new Thread( ts );
-				
+				threads[i] = new_thr;
 				new_thr.Start( i );
 			}
 			
-			Console.Write("Press any key to continue . . . ");
+			// Waiting for all of our threads...
+			foreach ( var thr in threads ) {
+				while ( thr.ThreadState != ThreadState.Stopped ) {
+					Thread.Sleep(50);
+				}
+			}
+			
+			logger.WriteEntry("Press any key to continue . . . ");
+			logger.Dispose();
 			Console.ReadKey(true);
 		}
 	}
